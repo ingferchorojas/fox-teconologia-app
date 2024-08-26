fcdefa7a920e46f14174568a3d3d1d1818ce67af
Fernando Rojas <frojas@monchis.com.py>
2024-08-26 20:29:05 +0000

    fix: register

--- src/app/pages/signup/signup.ts
+++ src/app/pages/signup/signup.ts
@@ -4,6 +4,7 @@ import { Router } from '@angular/router';
 import { UserData } from '../../providers/user-data';
 import { UserOptions } from '../../interfaces/user-options';
 import { App } from '@capacitor/app';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'page-signup',
@@ -17,7 +18,8 @@ export class SignupPage {
 
   constructor(
     public router: Router,
-    public userData: UserData
+    public userData: UserData,
+    public alertCtrl: AlertController // Inyecta AlertController
   ) {
     App.addListener('backButton', data => {
       if (data.canGoBack) {
@@ -28,7 +30,7 @@ export class SignupPage {
     });
   }
 
-  onSignup(form: NgForm) {
+  async onSignup(form: NgForm) {
     this.submitted = true;
     this.loading = true; // Mostrar indicador de carga
 
@@ -36,19 +38,43 @@ export class SignupPage {
       // Verificar si el nombre de usuario cumple con los requisitos
       const usernamePattern = /^[a-zA-Z0-9]+$/;
       if (!usernamePattern.test(this.signup.username)) {
-        alert('El nombre de usuario debe ser alfanumérico y no debe contener espacios.');
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: 'El nombre de usuario debe ser alfanumérico y no debe contener espacios.',
+          buttons: ['OK']
+        });
+        await alert.present();
         this.loading = false; // Ocultar indicador de carga
         return;
       }
 
-      this.userData.signup(this.signup.username, this.signup.password)
-        .then(() => {
-          this.router.navigateByUrl('/login');
-        })
-        .catch(error => {
-          this.loading = false
-          alert(error ?? 'Hubo un error al registrarse');
-        })
+      try {
+        await this.userData.signup(this.signup.username, this.signup.password);
+        
+        // Mostrar alerta de éxito
+        const alert = await this.alertCtrl.create({
+          header: 'Registro exitoso',
+          message: 'Tu registro fue exitoso. Ahora puedes iniciar sesión.',
+          buttons: [
+            {
+              text: 'OK',
+              handler: () => {
+                this.router.navigateByUrl('/login'); // Redirigir a la página de inicio de sesión
+              }
+            }
+          ]
+        });
+        await alert.present();
+      } catch (error) {
+        const alert = await this.alertCtrl.create({
+          header: 'Error',
+          message: error.message || 'Hubo un error al registrarse',
+          buttons: ['OK']
+        });
+        await alert.present();
+      } finally {
+        this.loading = false; // Ocultar indicador de carga
+      }
     } else {
       this.loading = false; // Ocultar indicador de carga si el formulario no es válido
     }
