3f2679690fd6c3de9c5ca012014c0203669e6fc8
Fernando Rojas <frojas@monchis.com.py>
2024-08-27 23:52:04 +0000

    fix: dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
 import { SwUpdate } from '@angular/service-worker';
 
-import { MenuController, Platform, ToastController } from '@ionic/angular';
+import { MenuController, Platform, ToastController, AlertController } from '@ionic/angular';
 
 import { StatusBar } from '@capacitor/status-bar';
 import { SplashScreen } from '@capacitor/splash-screen';
@@ -10,7 +10,6 @@ import { SplashScreen } from '@capacitor/splash-screen';
 import { Storage } from '@ionic/storage-angular';
 
 import { UserData } from './providers/user-data';
-
 
 @Component({
   selector: 'app-root',
@@ -20,29 +19,13 @@ import { UserData } from './providers/user-data';
 })
 export class AppComponent implements OnInit {
   isLoggedInPages = [
-    {
-      title: 'Clientes',
-      url: '/app/tabs/clients',
-      icon: 'people',
-    },
-    {
-      title: 'Productos',
-      url: '/app/tabs/products',
-      icon: 'storefront',
-    },
-    {
-      title: 'Venta',
-      url: '/app/tabs/cart',
-      icon: 'cart',
-    },
-    {
-      title: 'Información',
-      url: '/app/tabs/about',
-      icon: 'information-circle',
-    }
+    { title: 'Clientes', url: '/app/tabs/clients', icon: 'people' },
+    { title: 'Productos', url: '/app/tabs/products', icon: 'storefront' },
+    { title: 'Venta', url: '/app/tabs/cart', icon: 'cart' },
+    { title: 'Información', url: '/app/tabs/about', icon: 'information-circle' }
   ];
-  isLoggedOutPages = [
-  ];
+  
+  isLoggedOutPages = [];
   loggedIn = false;
   dark = false;
 
@@ -54,6 +37,7 @@ export class AppComponent implements OnInit {
     private userData: UserData,
     private swUpdate: SwUpdate,
     private toastCtrl: ToastController,
+    public alertCtrl: AlertController,
   ) {
     this.initializeApp();
   }
@@ -61,6 +45,7 @@ export class AppComponent implements OnInit {
   async ngOnInit() {
     await this.storage.create();
     await this.checkLoginStatus();
+    await this.loadDarkModePreference(); // Cargar la preferencia del modo oscuro
     this.listenForLoginEvents();
 
     this.swUpdate.versionUpdates.subscribe(async res => {
@@ -68,10 +53,7 @@ export class AppComponent implements OnInit {
         message: 'Update available!',
         position: 'bottom',
         buttons: [
-          {
-            role: 'cancel',
-            text: 'Reload'
-          }
+          { role: 'cancel', text: 'Reload' }
         ]
       });
 
@@ -84,6 +66,10 @@ export class AppComponent implements OnInit {
     });
   }
 
+  async ngAfterViewInit() {
+    await this.loadDarkModePreference();
+  }
+  
   initializeApp() {
     this.platform.ready().then(() => {
       if (this.platform.is('hybrid')) {
@@ -106,23 +92,41 @@ export class AppComponent implements OnInit {
   }
 
   listenForLoginEvents() {
-    window.addEventListener('user:login', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:signup', () => {
-      this.updateLoggedInStatus(true);
-    });
-
-    window.addEventListener('user:logout', () => {
-      this.updateLoggedInStatus(false);
-    });
+    window.addEventListener('user:login', () => this.updateLoggedInStatus(true));
+    window.addEventListener('user:signup', () => this.updateLoggedInStatus(true));
+    window.addEventListener('user:logout', () => this.updateLoggedInStatus(false));
   }
 
-  logout() {
-    this.userData.logout().then(() => {
-      return this.router.navigateByUrl('/login');
+  async loadDarkModePreference() {
+    const darkMode = await this.storage.get('darkMode');
+    this.dark = darkMode !== null ? darkMode : false;
+  }
+
+  async toggleDarkMode(event: CustomEvent | boolean) {
+    if (typeof event === 'boolean') {
+      // Si el evento es un booleano, actualiza directamente
+      this.dark = event;
+    } else if (event && event.detail) {
+      // Si es un CustomEvent, usa event.detail.checked
+      this.dark = event.detail.checked;
+    }
+  
+    await this.storage.set('darkMode', this.dark);
+  }
+
+  async logout() {
+    const alert = await this.alertCtrl.create({
+      header: 'Cerrar sesión',
+      message: '¿Estás seguro de que deseas cerrar sesión?',
+      buttons: [
+        { text: 'Cancelar', role: 'cancel', handler: () => console.log('Logout cancelado') },
+        { text: 'Cerrar sesión', handler: () => {
+          this.userData.logout();
+          this.router.navigateByUrl('/login');
+        }}
+      ]
     });
+    await alert.present();
   }
 
   openTutorial() {
